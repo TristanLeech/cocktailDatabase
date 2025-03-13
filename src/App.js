5319e4156d03d3dfbf6ab04fcf0fca9963c64f28
TristanLeech <241365@virtualwindow.co.za>
2025-03-13 02:03:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
 
+fetch("www.thecocktaildb.com/api/json/v1/1/random.php")
+  .then((response)=>response.json())
+  .then((data)=>{
+    console.log(data)
+    });
+
 function App() {
   return (
     <div className="App">
